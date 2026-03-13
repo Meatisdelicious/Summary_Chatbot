2aab3fa5553f42fe0e330a73623e86f3d36ede32
meatisdelicious <ssquerel@gmail.com>
2026-03-13 17:10:08 +0100

    adding image and updating tree

--- 1_notebooks/usefull_comments.py
+++ 1_notebooks/usefull_comments.py
@@ -4,6 +4,5 @@
 # run the frontend (using streamlit). Run the command in the app folder.
 # streamlit run app.py
 
-# other usefull commands : 
 # update project structure file :
 # tree -a --filelimit 100 --dirsfirst -I ".venv|.git|__pycache__|node_modules|.DS_Store" > project_structure.txt
