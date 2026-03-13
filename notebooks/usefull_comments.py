26b9886cb99a85fe7fd0e59dd6d538abbac3d7ff
meatisdelicious <ssquerel@gmail.com>
2026-03-13 14:51:20 +0100

    updating

--- notebooks/usefull_comments.py
+++ notebooks/usefull_comments.py
@@ -1,2 +1,7 @@
 # run in the summary_chatbot folder (when you can see the main)
 # uvicorn main:app --reload
+
+
+# other usefull commands : 
+# update project structure file :
+# tree -a --filelimit 100 --dirsfirst -I ".venv|.git|__pycache__|node_modules|.DS_Store" > project_structure.txt
